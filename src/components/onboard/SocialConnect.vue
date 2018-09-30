68662f238ef97404e51928e15686fabf66211a2a
Your Name <you@example.com>
2018-09-30 20:38:30 +0300

    vuex store, saving schedule in store, reviewdata and social connect components

--- src/components/onboard/SocialConnect.vue
+++ src/components/onboard/SocialConnect.vue
@@ -1,5 +1,51 @@
 <template>
-  <div>social conenct</div>
+  <div class="component-container">
+    <div class="component-intro-text">
+      <h1 class="intro-title">Connect rankingCoach to your social media accounts</h1>
+      <p>Social media links make your website even more relevant and interactive. If you already have social media accounts for your business, you can manually connect them now.</p>
+    </div>
+    <div class="social-wrapper row">
+      <div class="col-lg-4 social-block">
+        <div class="social-media-icon">
+          <img src="../../assets/facebookico.svg" alt="facebook">
+        </div>
+        <h5 class="social-media-title">
+          Facebook
+        </h5>
+        <p class="social-media-user">
+          Connected as: <b>@tomssteakhouse</b>
+        </p>
+        <button class="button button-secondary">DISCONNECT ACCOUNT</button> 
+      </div>
+      <div class="col-lg-4 social-block">
+        <div class="social-media-icon">
+          <img src="../../assets/twitter.svg" alt="twitter">
+        </div>
+        <h5 class="social-media-title">
+          Twitter
+        </h5>
+        <p class="social-media-user">
+          Connected as: <b>@tomssteakhouse</b>
+        </p>
+        <button class="button button-secondary">DISCONNECT ACCOUNT</button> 
+      </div>
+      <div class="col-lg-4 social-block">
+        <div class="social-media-icon">
+          <img src="../../assets/gplus.svg" alt="gplus">
+        </div>
+        <h6 class="social-media-title">
+          Google Plus
+        </h6>
+        <p class="social-media-user">
+        </p>
+        <button class="button button-primary">CONNECT ACCOUNT</button> 
+      </div>
+    </div>
+    <div class="action-buttons">
+      <router-link v-bind:to="'/images'"><button class="button button-secondary"><i class="material-icons">arrow_back</i>Back</button></router-link>
+      <router-link v-bind:to="'/review'"><button class="button button-primary">Next<i class="material-icons">arrow_forward</i></button></router-link>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -9,6 +55,40 @@ export default {
 </script>
 
 <style lang="scss">
+@import "../../style/base.scss";
+
+.intro-title {
+  color: $brand-primary-color;
+  font-size: $f-size-xl;
+}
+
+.social-wrapper {
+  margin-top: 100px;
+}
+
+.social-block {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    > * {
+      margin-bottom: $spacing-m;
+    }
+
+    .button {
+      margin-top: auto;
+    }
+
+    img {
+      width: 50px;
+      height: 50px;
+    }
+}
+
+.social-media-title {
+  text-transform: uppercase;
+  color: $brand-secondary-color;
+}
 
 </style>
 
--- src/components/onboard/UploadImages.vue
+++ src/components/onboard/UploadImages.vue
@@ -7,8 +7,20 @@
     <div class="container-images row">
       <div class="col-lg-6 image-wrapper">
         <div class="card-image">
-          <div class="card-image-container">
+          <div class="card-image-container hidden-icons-container">
            <img src="../../assets/steaklogo.jpeg" alt="logo">
+
+           <!-- edit and delete button displayed only on image hover -->
+           <div class="icons-hidden">
+             <div class="icons-hidden-wrapper">
+               <i class="material-icons">edit</i>
+               <p>Barbeiten</p>
+             </div>
+             <div class="icons-hidden-wrapper">
+               <i class="material-icons">delete</i>
+               <p>LOSCHEN</p>
+             </div>
+           </div>
           </div>
         </div>
         <div class="help-text-container">
@@ -36,6 +48,30 @@
         </div>
       </div>
     </div>
+    <div class="component-container">
+      <h4 class="section-title">Youtube url</h4>
+      <p>Some directories can display video. Some directories need to have videos uploaded to them, others may only show embedded YouTube videos. It is recommended to upload a video and paste a youtube link to a video about your business</p>
+      <div class="video-container">
+        <div class="video-thumbnail">
+          <img src="../../assets/tomsteak.png" alt="video">
+          <i class="material-icons play-icon">play_circle_outline</i>
+        </div>
+        <div class="video-details">
+          <div class="video-link">
+            <h6>URL</h6>
+            <input type="text" id="url" name="url" class="input-details"/>
+          </div>
+          <div class="video-description">
+            <h6>Description</h6>
+            <textarea rows="4" cols="50" name="comment" maxlength="250" class="input-details"></textarea>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="action-buttons">
+      <router-link v-bind:to="'/open-hours'"><button class="button button-secondary"><i class="material-icons">arrow_back</i>Back</button></router-link>
+      <router-link v-bind:to="'/connect'"><button class="button button-primary">Next<i class="material-icons">arrow_forward</i></button></router-link>
+    </div>
   </div>
 </template>
 
@@ -45,7 +81,7 @@ export default {
 }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 @import "../../style/base.scss";
 
 .container-images {
@@ -72,17 +108,119 @@ export default {
   height: 218px;
 
   .card-image-container {
+    background-color: #000000;
     display: flex;
     align-items: center;
     justify-content: center;
     overflow: hidden;
+    position: relative;
     width: 100%;
     height: 100%;
+
+    .icons-hidden {
+      display: none;
+
+      .material-icons {
+        color: $brand-light-color;
+        font-size: $f-size-xxl;
+      }
+    }
+
+    &.hidden-icons-container:hover {
+       p {
+         color: $light-gray-color-xxs;
+       }
+
+      .icons-hidden {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        position: absolute
+      }
+
+      img {
+        opacity: 0.5;
+      }
+    }
 
     img {
         height: 100%;
         width: auto;
       }
+
+      .icons-hidden-wrapper {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+
+        p {
+          font-size: $f-size-s;
+        }
+      }
+
+      .icons-hidden-wrapper:not(:last-of-type) {
+        margin-right: $spacing-xl;
+        position: relative;
+
+        &::after {
+          position: absolute;
+          content: '';
+          width: 1px;
+          height: 40px;
+          right: -17px;
+          top: 7%;
+          background-color: $brand-light-color;
+        }
+      }
+  }
+}
+
+.component-container {
+  margin-top: $spacing-xxxl;
+}
+
+.video-container {
+   margin-top: $spacing-xxxl;
+   display: flex;
+}
+
+.video-details {
+  display: flex;
+  flex-direction: column; 
+  justify-content: space-between;
+  flex: 1;
+  margin-left: $spacing-xl;
+
+  .input-details {
+    font-family: $font-family-base;
+    font-size: $f-size;
+    color: $brand-secondary-color;
+    padding: 12px 8px;
+    width: 100%;
+  }
+}
+
+.video-thumbnail {
+  display: flex;
+  align-items: center;
+  background-color: #000000;
+  overflow: hidden;
+  position: relative; 
+  width: 218px;
+  height: 218px;
+
+  .play-icon {
+    position: absolute;
+    left: 50%;
+    font-size: $f-size-xl;
+    transform: translateX(-50%);
+    color: $brand-light-color;
+  }
+
+  img {
+    width: 100%;
+    height: auto;
+    opacity: 0.4;
   }
 }
 </style>
